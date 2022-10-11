276f1105cacaa831a1c6c9faffa669a08506f26a
HermanoCastro65 <hermanocastro18@gmail.com>
2022-10-11 16:10:25 -0300

    fix change function name for interpreter

--- src/core/interpreter/app/interpreter.app.ts
+++ src/core/interpreter/app/interpreter.app.ts
@@ -4,7 +4,7 @@ import { IRuntime } from '@models/runtime.export'
 import { parse } from '@core/parser.export'
 import { executeProc } from '@core/interpreter.export'
 
-export const runtime: IRuntime = async (src, env) => () => {
+export const interpreter: IRuntime = async (src, env) => () => {
   const tokens = tokenize(src)
   const program = parse(tokens)
   const transformedProgram = transformer(program)
