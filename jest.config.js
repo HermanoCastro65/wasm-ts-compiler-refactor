6bdaffea605ef31c56697098fdaa65e49c5fc924
HermanoCastro65 <hermanocastro18@gmail.com>
2022-10-11 14:49:15 -0300

    add modules name mapper on jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@parser-types/(.*)$': '<rootDir>/src/models/parser/types/$1',
+  },
 }
