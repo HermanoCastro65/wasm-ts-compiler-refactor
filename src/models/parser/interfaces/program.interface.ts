92f524b6f6b48f04a6959b3c884ad863b5e68034
HermanoCastro65 <hermanocastro18@gmail.com>
2022-10-11 11:59:36 -0300

    refact iprogram interface with value and operator as optional atributtes

--- src/models/parser/interfaces/program.interface.ts
+++ src/models/parser/interfaces/program.interface.ts
@@ -1,3 +1,7 @@
+import { TExpression, TOperator } from '@parser-types/expression.exports'
+
 export interface IProgram {
   type: string
+  operator?: TOperator
+  value?: TExpression | string | number
 }
